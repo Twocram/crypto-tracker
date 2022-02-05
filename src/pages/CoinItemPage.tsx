0ad15b16495d276f4b70c4efe32facbb0ea8e5e0
Twocram <2002maslovartem@gmail.com>
2022-02-05 16:21:36 +0500

    added types and added back button

--- src/pages/CoinItemPage.tsx
+++ src/pages/CoinItemPage.tsx
@@ -1,26 +1,28 @@
 import React, { FC, useState, useEffect } from 'react';
-import { ICoin } from '../types/coin';
+import { TCoinItem } from '../types/coin';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { CoinItemPageParams } from '../types/coin';
 
 const CoinItemPage: FC = () => {
-  const [coin, setCoin] = useState<ICoin | null>(null);
+  const [coin, setCoin] = useState<TCoinItem | null>(null);
+  const navigate = useNavigate();
   const params = useParams<CoinItemPageParams>();
   useEffect(() => {
     fetchUser();
-  }, []);
+  });
 
   async function fetchUser() {
-    const response = await axios.get<ICoin>(
+    const response = await axios.get<TCoinItem>(
       'https://api.coingecko.com/api/v3/coins/' + params.id
     );
     setCoin(response.data);
   }
   return (
     <div className='flex'>
+      <button onClick={() => navigate('/')}>Back</button>
       <h1>{coin?.name}</h1>
-      <img src={coin?.image} alt={coin?.symbol} />
+      <img src={coin?.image.small} alt={coin?.symbol} />
     </div>
   );
 };
--- src/types/coin.tsx
+++ src/types/coin.tsx
@@ -10,3 +10,26 @@ export interface ICoin {
 export type CoinItemPageParams = {
   id: string;
 };
+
+export type TCoinItem = {
+  id: string;
+  symbol: string;
+  name: string;
+  image: TCoinItemImage;
+  market_data: TCoinItemMarketData;
+};
+
+type TCoinItemImage = {
+  thumb: string;
+  small: string;
+  large: string;
+};
+
+type TCoinItemMarketData = {
+  current_price: {
+    usd: number;
+  };
+  market_cup: {
+    usd: number;
+  };
+};
