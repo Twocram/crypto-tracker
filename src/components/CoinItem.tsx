c070a4c9a40d56f096aba02d56ac4605e61d4c01
Twocram <2002maslovartem@gmail.com>
2022-02-05 16:54:59 +0500

    added transition between pages

--- src/components/CoinItem.tsx
+++ src/components/CoinItem.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { ICoin } from '../types/coin';
-
+import { useNavigate } from 'react-router-dom';
 interface CoinItemProps {
   coin: ICoin;
 }
 
 const CoinItem: FC<CoinItemProps> = ({ coin }) => {
+  const navigate = useNavigate();
   return (
     <div className='flex'>
       <img className='mr-3' src={coin.image} alt={coin.symbol} />
@@ -13,6 +14,12 @@ const CoinItem: FC<CoinItemProps> = ({ coin }) => {
       <span className='mr-3'>{coin.symbol}</span>
       <span className='mr-3'>{coin.current_price}</span>
       <span className='mr-3'>{coin.market_cap}</span>
+      <button
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+          navigate(`/${coin.id}`)
+        }>
+        Go to page
+      </button>
     </div>
   );
 };
--- src/pages/CoinItemPage.tsx
+++ src/pages/CoinItemPage.tsx
@@ -9,18 +9,22 @@ const CoinItemPage: FC = () => {
   const navigate = useNavigate();
   const params = useParams<CoinItemPageParams>();
   useEffect(() => {
+    async function fetchUser() {
+      const response = await axios.get<TCoinItem>(
+        'https://api.coingecko.com/api/v3/coins/' + params.id
+      );
+      setCoin(response.data);
+    }
     fetchUser();
-  });
+  }, []);
 
-  async function fetchUser() {
-    const response = await axios.get<TCoinItem>(
-      'https://api.coingecko.com/api/v3/coins/' + params.id
-    );
-    setCoin(response.data);
+  function getBack(e: React.MouseEvent<HTMLButtonElement>) {
+    setCoin(null);
+    navigate('/');
   }
   return (
     <div className='flex'>
-      <button onClick={() => navigate('/')}>Back</button>
+      <button onClick={getBack}>Back</button>
       <h1>{coin?.name}</h1>
       <img src={coin?.image.small} alt={coin?.symbol} />
     </div>
