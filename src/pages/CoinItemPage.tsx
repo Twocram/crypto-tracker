f970e3613992640d7329cc94d5552c78ffcb20e6
Twocram <2002maslovartem@gmail.com>
2022-04-09 19:17:53 +0500

    added loading caption

--- src/pages/CoinItemPage.tsx
+++ src/pages/CoinItemPage.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { TCoinItem, CoinItemPageParams } from '../types/coin';
-import axios from 'axios';
+import React, { FC, useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { TCoinItem, CoinItemPageParams } from "../types/coin";
+import axios from "axios";
 
 const CoinItemPage: FC = () => {
   const [coin, setCoin] = useState<TCoinItem | null>(null);
@@ -11,7 +11,7 @@ const CoinItemPage: FC = () => {
   useEffect(() => {
     async function fetchCoin() {
       const response = await axios.get<TCoinItem>(
-        'https://api.coingecko.com/api/v3/coins/' + params.id
+        "https://api.coingecko.com/api/v3/coins/" + params.id
       );
       setCoin(response.data);
     }
@@ -22,38 +22,45 @@ const CoinItemPage: FC = () => {
 
   function getBack(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    navigate('/');
+    navigate("/");
   }
   return (
-    <div className='flex flex-col'>
-      <button
-        className='text-black bg-white rounded-sm p-2 w-24'
-        onClick={getBack}>
-        Back
-      </button>
-      <div className='flex flex-col items-center justify-center'>
-        <img
-          src={coin?.image.small}
-          className='w-[72px] h-[72px]'
-          alt={coin?.symbol}
-        />
-        <h1 className='text-xl font-bold'>
-          {coin?.name} ({coin?.symbol})
-        </h1>
-        <span className='text-xl'>
-          Current Price: {coin?.market_data.current_price.usd}$
-        </span>
-        <span className='text-xl'>
-          Total Price: {coin?.market_data.market_cap.usd}$
-        </span>
-        <span className='text-xl'>
-          Price change percentage 24h:{' '}
-          <span>
-            {coin?.market_data.price_change_percentage_24h.toFixed(2)}
-          </span>
-        </span>
+    <>
+      <div className="flex flex-col">
+        <button
+          className="text-black bg-white rounded-sm p-2 w-24"
+          onClick={getBack}
+        >
+          Back
+        </button>
+        {coin ? (
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={coin?.image.small}
+              className="w-[72px] h-[72px]"
+              alt={coin?.symbol}
+            />
+            <h1 className="text-xl font-bold">
+              {coin?.name} ({coin?.symbol})
+            </h1>
+            <span className="text-xl">
+              Current Price: {coin?.market_data.current_price.usd}$
+            </span>
+            <span className="text-xl">
+              Total Price: {coin?.market_data.market_cap.usd}$
+            </span>
+            <span className="text-xl">
+              Price change percentage 24h:{" "}
+              <span>
+                {coin?.market_data.price_change_percentage_24h.toFixed(2)}
+              </span>
+            </span>
+          </div>
+        ) : (
+          <div className={"flex justify-center text-2xl"}>Loading...</div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,14 +1,14 @@
-import React, { FC, useEffect, useState } from 'react';
-import axios from 'axios';
-import { ICoin } from '../types/coin';
-import List from '../components/List';
-import CoinItem from '../components/CoinItem';
+import React, { FC, useEffect, useState } from "react";
+import axios from "axios";
+import { ICoin } from "../types/coin";
+import List from "../components/List";
+import CoinItem from "../components/CoinItem";
 
 interface CoinsProps {}
 
 const Coins: FC<CoinsProps> = () => {
   const [coins, setCoins] = useState<ICoin[]>([]);
-  const [search, setSearch] = useState<string>('');
+  const [search, setSearch] = useState<string>("");
   useEffect(() => {
     setTimeout(() => {
       fetchCoins();
@@ -25,27 +25,33 @@ const Coins: FC<CoinsProps> = () => {
 
   async function fetchCoins() {
     const response = await axios.get<ICoin[]>(
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
     );
     setCoins(response.data);
   }
   return (
-    <div className=''>
-      <div className='flex flex-col justify-center items-center'>
+    <>
+      <div className="flex flex-col justify-center items-center">
         <h2>Check coin stats</h2>
         <input
-          className='w-80 mt-3 p-3 outline-none bg-gradient-to-r from-violet-500 to-indigo-600'
-          type='text'
+          className="w-80 mt-3 p-3 outline-none bg-gradient-to-r from-violet-500 to-indigo-600"
+          type="text"
           onChange={changeHandler}
-          placeholder='Type name of crypto'
+          placeholder="Type name of crypto"
         />
       </div>
 
-      <List
-        items={filteredCoins}
-        renderItem={(coin: ICoin) => <CoinItem coin={coin} key={coin.id} />}
-      />
-    </div>
+      {coins.length > 0 ? (
+        <List
+          items={filteredCoins}
+          renderItem={(coin: ICoin) => <CoinItem coin={coin} key={coin.id} />}
+        />
+      ) : (
+        <div className={"flex justify-center mt-[50px] text-2xl"}>
+          Loading...
+        </div>
+      )}
+    </>
   );
 };
 
